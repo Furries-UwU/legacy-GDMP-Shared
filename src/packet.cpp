e980653b3cf9a3b22a16cfac8b43adee2fd008f1
xhayper <hayper1919@gmail.com>
2022-05-11 14:13:51 +0700

    stuff

--- src/packet.cpp
+++ src/packet.cpp
@@ -1,12 +1,12 @@
 #include "packet.hpp"
 
-void sendPacket(ENetPeer *peer, Packet packet) {
-    ENetPacket *enetPacket = enet_packet_create(nullptr, sizeof(packet), ENET_PACKET_FLAG_RELIABLE);
+void sendPacket(ENetPeer *peer, Packet packet, unsigned int packetLength) {
+    ENetPacket *enetPacket = enet_packet_create(nullptr, packetLength, ENET_PACKET_FLAG_RELIABLE);
     if (!enetPacket)
         return;
 
     memcpy(enetPacket->data, &packet.type, 1);
-    memcpy(enetPacket->data + 1, &packet.data, sizeof(packet.data));
+    memcpy(enetPacket->data + 1, packet.data, packetLength-1);
 	
     if (enet_peer_send(peer, 0, enetPacket) != 0)
         enet_packet_destroy(enetPacket);
