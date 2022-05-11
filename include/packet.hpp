8693bb4a7795e991fba6633e66eca248a24a861d
xhayper <hayper1919@gmail.com>
2022-05-11 13:53:18 +0700

    feat: bro

--- include/packet.hpp
+++ include/packet.hpp
@@ -5,7 +5,8 @@
 
 struct Packet {
 	uint8_t type;
-	char* data;
+	unsigned int length;
+	void* data;
 };
 
 void sendPacket(ENetPeer *peer, Packet packet, unsigned int packetLength);
--- src/packet.cpp
+++ src/packet.cpp
@@ -1,11 +1,11 @@
 #include "packet.hpp"
 
-void sendPacket(ENetPeer *peer, Packet packet, unsigned int packetLength) {
-    ENetPacket *enetPacket = enet_packet_create(nullptr, packetLength, ENET_PACKET_FLAG_RELIABLE);
+void sendPacket(ENetPeer *peer, Packet packet) {
+    ENetPacket *enetPacket = enet_packet_create(&packet, sizeof(packet), ENET_PACKET_FLAG_RELIABLE);
     if (!enetPacket)
         return;
 
-	memcpy(enetPacket->data, &packet, packetLength);
+	//memcpy(enetPacket->data, &packet, sizeof(packet));
 	
     if (enet_peer_send(peer, 0, enetPacket) != 0)
         enet_packet_destroy(enetPacket);
