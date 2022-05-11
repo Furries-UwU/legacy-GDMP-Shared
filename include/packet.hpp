987f615312251ce421bc476a318c793424df9aef
xhayper <hayper1919@gmail.com>
2022-05-11 12:43:29 +0700

    w

--- include/packet.hpp
+++ include/packet.hpp
@@ -6,15 +6,6 @@
 struct Packet {
     uint8_t type;
     uint8_t* data;
-
-    Packet(uint8_t packetType) {
-        this->type = packetType;
-    }
-
-    Packet(uint8_t packetType, uint8_t* packetData) {
-        this->type = packetType;
-        this->data = packetData;
-    }
 };
 
 void sendPacket(ENetPeer *peer, Packet packet, unsigned int packetLength);
