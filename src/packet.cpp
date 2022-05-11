8f5f1a3d258aac0107ff340077e9a4f2032ae468
xhayper <hayper1919@gmail.com>
2022-05-11 22:20:16 +0700

    feat: Stuff

--- src/packet.cpp
+++ src/packet.cpp
@@ -7,18 +7,25 @@ Packet::Packet(uint8_t type, uint32_t length, uint8_t* data) {
 }
 
 const uint8_t& Packet::operator[](int index) const {
+    uint8_t bytes[this->length];
+    bytes[0] = this->type;
+	bytes[1] = (this->length >> 24) & 0xFF;
+	bytes[2] = (this->length >> 16) & 0xFF;
+	bytes[3] = (this->length >> 8) & 0xFF;
+	bytes[4] = this->length & 0xFF;
+	memcpy(bytes + 5, this->data, this->length - 5);
+
+	/*
     if (!index) {
         return this->type;
     }
-    else if (index < sizeof(this->length)) {
-        return (this->length >> (8 * index - 1)) & 0xFF;
-    }
     else if (index < this->length) {
-        return this->data[index - (1 + sizeof(this->length))];
+        return this->data[index - 1];
     }
     else {
         throw std::range_error("Out of packet range");
     }
+    */
 }
 
 void sendPacket(ENetPeer* peer, Packet packet) {
