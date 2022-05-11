3b6aea7aba12c026bb5ec6176e1222a8d5de4a68
xhayper <hayper1919@gmail.com>
2022-05-11 13:15:46 +0700

    feat: test

--- src/packet.cpp
+++ src/packet.cpp
@@ -4,6 +4,8 @@ void sendPacket(ENetPeer *peer, char* data) {
     ENetPacket *enetPacket = enet_packet_create(data, strlen(data)+1, ENET_PACKET_FLAG_RELIABLE);
     if (!enetPacket)
         return;
+
+	memcpy(enetPacket->data, data, strlen(data)+1);
 	
     if (enet_peer_send(peer, 0, enetPacket) != 0)
         enet_packet_destroy(enetPacket);
