9eedeb0466abddbef6d83ee8f03cd8ed147ec5bf
xhayper <hayper1919@gmail.com>
2022-05-11 12:31:03 +0700

    feat: stuff ig

--- src/packet.cpp
+++ src/packet.cpp
@@ -4,8 +4,10 @@ void sendPacket(ENetPeer *peer, Packet packet, unsigned int packetLength) {
     ENetPacket *enetPacket = enet_packet_create(nullptr, packetLength, ENET_PACKET_FLAG_RELIABLE);
     if (!enetPacket)
         return;
+
+    uint8_t* data = (uint8_t*)&packet;
 	
-    memcpy(enetPacket->data, &packet, packetLength);
+    memcpy(enetPacket->data, &data, packetLength);
 	
     if (enet_peer_send(peer, 0, enetPacket) != 0)
         enet_packet_destroy(enetPacket);
