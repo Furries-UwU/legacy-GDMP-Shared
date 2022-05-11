c0e970b3a08b7edacf119644b11c157e45e1eb87
xhayper <hayper1919@gmail.com>
2022-05-11 20:46:55 +0700

    feat: stuff

--- include/packet.hpp
+++ include/packet.hpp
@@ -6,6 +6,10 @@
 struct Packet {
 	uint8_t type;
 	uint8_t* data;
+	unsigned int length;
+
+	Packet(uint8_t type, uint8_t* data, unsigned int length);
+	const uint8_t& operator[](int index) const;
 };
 
 void sendPacket(ENetPeer* peer, Packet packet, unsigned int packetLength);
--- src/packet.cpp
+++ src/packet.cpp
@@ -1,14 +1,27 @@
 #include "packet.hpp"
 
+Packet::Packet(uint8_t type, uint8_t* data, unsigned int length) {
+	this->type = type;
+	this->data = data;
+	this->length = length + 1;
+}
+
+const uint8_t& Packet::operator[](int index) const {
+    if (!index) {
+        return this->type;
+    }
+    else if (index < this->length) {
+		return this->data[index - 1];
+    }
+    else {
+        throw std::range_error("Out of packet range");
+    }
+}
+
 void sendPacket(ENetPeer* peer, Packet packet, unsigned int packetLength) {
-    ENetPacket* enetPacket = enet_packet_create(nullptr, packetLength, ENET_PACKET_FLAG_RELIABLE);
+    ENetPacket* enetPacket = enet_packet_create(&packet, packetLength, ENET_PACKET_FLAG_RELIABLE);
     if (!enetPacket)
         return;
-
-    unsigned int packetTypeSize = sizeof(packet.type);
-
-    memcpy(enetPacket->data, &packet.type, packetTypeSize);
-    memcpy(enetPacket->data + packetTypeSize, packet.data, packetLength - packetTypeSize);
 
     if (enet_peer_send(peer, 0, enetPacket) != 0)
         enet_packet_destroy(enetPacket);
