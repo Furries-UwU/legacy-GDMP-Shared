4bb0080ebb82ae99782a9bf98581e480bd054e5b
xhayper <hayper1919@gmail.com>
2022-05-11 20:59:36 +0700

    feat: what

--- src/packet.cpp
+++ src/packet.cpp
@@ -19,9 +19,12 @@ const uint8_t& Packet::operator[](int index) const {
 }
 
 void sendPacket(ENetPeer* peer, Packet packet, unsigned int packetLength) {
-    ENetPacket* enetPacket = enet_packet_create(&packet, packetLength, ENET_PACKET_FLAG_RELIABLE);
+    ENetPacket* enetPacket = enet_packet_create(nullptr, packetLength, ENET_PACKET_FLAG_RELIABLE);
     if (!enetPacket)
         return;
+
+    memcpy(enetPacket->data, &packet.type, 1);
+	memcpy(enetPacket->data + 1, &packet.data, packet.length);
 
     if (enet_peer_send(peer, 0, enetPacket) != 0)
         enet_packet_destroy(enetPacket);
