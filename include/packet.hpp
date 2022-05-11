2f8f3111f26ed81d9a308185e8dec9da6ed55eb7
xhayper <hayper1919@gmail.com>
2022-05-11 12:51:22 +0700

    wafskinj

--- include/packet.hpp
+++ include/packet.hpp
@@ -3,9 +3,11 @@
 #include <stdexcept>
 #include <stdint.h>
 
+#pragma pack(push, 1)
 struct Packet {
     uint8_t type;
     uint8_t* data;
 };
+#pragma pack(pop)
 
 void sendPacket(ENetPeer *peer, Packet packet, unsigned int packetLength);
--- src/packet.cpp
+++ src/packet.cpp
@@ -4,10 +4,8 @@ void sendPacket(ENetPeer *peer, Packet packet, unsigned int packetLength) {
     ENetPacket *enetPacket = enet_packet_create(nullptr, packetLength, ENET_PACKET_FLAG_RELIABLE);
     if (!enetPacket)
         return;
-
-    uint8_t* data = (uint8_t*)&packet;
 	
-    memcpy(enetPacket->data, &data, packetLength);
+    memcpy(enetPacket->data, &packet, packetLength);
 	
     if (enet_peer_send(peer, 0, enetPacket) != 0)
         enet_packet_destroy(enetPacket);
