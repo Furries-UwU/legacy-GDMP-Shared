517a966fe7c943fc842835288f45de989ba4ccd4
xhayper <hayper1919@gmail.com>
2022-05-12 11:43:25 +0700

    feat: Stuff

--- src/packet.cpp
+++ src/packet.cpp
@@ -1,5 +1,12 @@
 #include "packet.hpp"
 
+uint8_t* u32_to_u8(const uint32_t u32, uint8_t* u8) {
+    u8[0] = (u32 & 0xff000000) >> 24;
+    u8[1] = (u32 & 0x00ff0000) >> 16;
+    u8[2] = (u32 & 0x0000ff00) >> 8;
+    u8[3] = u32 & 0x000000ff;
+}
+
 Packet::Packet(uint8_t type) {
     this->type = type;
     this->length = 0;
@@ -17,7 +24,10 @@ const uint8_t& Packet::operator[](int index) {
         return this->type;
     }
     else if (index < 5) {
-        return *((uint8_t*)&this->length)+index-1;
+        uint8_t* data = (uint8_t*) malloc(4);
+		u32_to_u8(this->length, data);
+		return data[index - 1];
+        //return *((uint8_t*)&this->length) + index - 1;
     }
     else if (index < this->length + 5) {
         return this->data[index - 5];
