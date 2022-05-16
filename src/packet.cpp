c10cb2f5d7748cb499c126ac362ffcd210235256
xhayper <hayper1919@gmail.com>
2022-05-16 22:21:28 +0700

    feat: Stuff

--- src/packet.cpp
+++ src/packet.cpp
@@ -29,26 +29,27 @@ Packet Packet::serialize(ENetPacket *eNetPacket) {
 void Packet::sendPacket(ENetPeer *peer) const {
     ENetPacket *packet = enet_packet_create(nullptr, this->length + 5, ENET_PACKET_FLAG_RELIABLE);
 
-    packet->data[0] = this->type;
-    packet->data[1] = (this->length >> 24) & 0xFF;
-    packet->data[2] = (this->length >> 16) & 0xFF;
-    packet->data[3] = (this->length >> 8) & 0xFF;
-    packet->data[4] = this->length & 0xFF;
-    memcpy(packet->data + 5, this->data, this->length);
+    uint8_t* data = new uint8_t[length];
 
+    for (unsigned int i = 0; i < length; i++) {
+        data[i] = this->operator[](i);
+    }
 
     if (enet_peer_send(peer, 0, packet) != 0)
         enet_packet_destroy(packet);
 }
 
-const uint8_t &Packet::operator[](int index) {
+const uint8_t& Packet::operator[](int index) {
     if (!index) {
         return this->type;
-    } else if (index < 5) {
-        return reinterpret_cast<uint8_t *>(&this->length)[index - 1];
-    } else if (index < this->length + 5) {
+    }
+    else if (index < 5) {
+        return reinterpret_cast<uint8_t*>(&this->length)[index - 1];
+    }
+    else if (index < this->length + 5) {
         return this->data[index - 5];
-    } else {
+    }
+    else {
         throw std::out_of_range("Out of packet range");
     }
 }
