fb154cd8734e7712ea84db328b039773525831ee
xhayper <hayper1919@gmail.com>
2022-06-01 22:10:31 +0700

    feat: FIX SHIT UOIHAFDUHOIASDFUOJISHUJN(IHNYUIJAS)

--- include/packetUtility.hpp
+++ include/packetUtility.hpp
@@ -1,11 +1,10 @@
 #pragma once
-#include <steam/steamnetworkingsockets.h>
-#include <steam/isteamnetworkingutils.h>
+#include <enet/enet.h>
 #include <packet.pb.h>
 #include <vector>
 
 class PacketUtility {
 public:
-    static void sendPacket(ISteamNetworkingSockets*, HSteamNetConnection, const Packet&);
-    static void sendPacket(ISteamNetworkingSockets*, HSteamNetConnection, const IncomingPacket&);
+    static void sendPacket(ENetPeer*, const Packet&);
+    static void sendPacket(ENetPeer*, const IncomingPacket&);
 };
--- src/packetUtility.cpp
+++ src/packetUtility.cpp
@@ -1,18 +1,23 @@
 #include "packetUtility.hpp"
 
-
-void PacketUtility::sendPacket(ISteamNetworkingSockets* interface, HSteamNetConnection connection, const Packet &packet)
-{
-    if (interface == nullptr)
+void PacketUtility::sendPacket(ENetPeer *peer, const Packet& packet) {
+    if (peer == nullptr)
         return;
-    const char* pData = packet.SerializeAsString().c_str();
-    interface->SendMessageToConnection(connection, pData, (uint32)strlen(pData), k_nSteamNetworkingSend_Reliable, nullptr);
+
+    const char* data = packet.SerializeAsString().c_str();
+
+    ENetPacket *enetPacket = enet_packet_create(data, strlen(data), ENET_PACKET_FLAG_RELIABLE);
+    if (enet_peer_send(peer, 0, enetPacket) != 0)
+        enet_packet_destroy(enetPacket);
 }
 
-void PacketUtility::sendPacket(ISteamNetworkingSockets* interface, HSteamNetConnection connection, const IncomingPacket &packet)
-{
-    if (interface == nullptr)
+void PacketUtility::sendPacket(ENetPeer *peer, const IncomingPacket& packet) {
+    if (peer == nullptr)
         return;
-    const char* pData = packet.SerializeAsString().c_str();
-    interface->SendMessageToConnection(connection, pData, (uint32)strlen(pData), k_nSteamNetworkingSend_Reliable, nullptr);
+
+    const char* data = packet.SerializeAsString().c_str();
+
+    ENetPacket *enetPacket = enet_packet_create(data, strlen(data), ENET_PACKET_FLAG_RELIABLE);
+    if (enet_peer_send(peer, 0, enetPacket) != 0)
+        enet_packet_destroy(enetPacket);
 }
